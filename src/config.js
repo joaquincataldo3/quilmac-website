277f9640379f65e084484bb5012c2a5be6ec8fb6
joaquincataldo3 <joaco.cataldo3@gmail.com>
2023-02-14 23:00:04 -0300

    changing module.exports structure

--- src/config.js
+++ src/config.js
@@ -5,4 +5,4 @@ const DB_USER = process.env.DB_USER || "root"
 const DB_PASSWORD = process.env.DB_PASSWORD || null
 const DB_NAME = process.env.DB_NAME || "quilmac_db_ralway"
 
-module.exports = PORT, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME;
+module.exports = {PORT, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME};
