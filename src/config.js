a0f664cd118db3c82ac1f184487be9be10492ea7
joaquincataldo3 <joaco.cataldo3@gmail.com>
2023-02-14 22:54:56 -0300

    changing export

--- src/config.js
+++ src/config.js
@@ -1,5 +1,8 @@
-export const PORT = process.env.PORT || 3095
-export const DB_HOST = process.env.DB_HOST || "localhost"
-export const DB_USER = process.env.DB_USER || "root"
-export const DB_PASSWORD = process.env.DB_PASSWORD || null
-export const DB_NAME = process.env.DB_NAME || "quilmac_db_ralway"
+
+const PORT = process.env.PORT || 3095
+const DB_HOST = process.env.DB_HOST || "localhost"
+const DB_USER = process.env.DB_USER || "root"
+const DB_PASSWORD = process.env.DB_PASSWORD || null
+const DB_NAME = process.env.DB_NAME || "quilmac_db_ralway"
+
+module.exports = PORT, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME;
