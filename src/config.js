fc23d2205482f2c5c27fd13d31de805cd35e996a
joaquincataldo3 <joaco.cataldo3@gmail.com>
2023-02-14 22:42:58 -0300

    changing config.js

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@ export const PORT = process.env.PORT || 3095
 export const DB_HOST = process.env.DB_HOST || "localhost"
 export const DB_USER = process.env.DB_USER || "root"
 export const DB_PASSWORD = process.env.DB_PASSWORD || null
-export const DB_NAME = process.env.DB_NAME || "quilmac_db"
+export const DB_NAME = process.env.DB_NAME || "quilmac_db_ralway"
