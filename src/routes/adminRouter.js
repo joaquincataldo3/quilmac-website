e77073511cce227e19c6ce2dd151efe0aedf37fa
joaquincataldo3 <joaco.cataldo3@gmail.com>
2023-02-17 16:19:55 -0300

    organizing routes

--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -2,16 +2,28 @@ const express = require('express');
 const adminController = require('../controllers/adminController');
 const registerValidations = require("../middlewares/registerValidation");
 const loginValidations = require("../middlewares/loginValidation");
+const accessoryCreationValidation = require("../middlewares/accessoryCreationValidation");
+const deviceCreationValidation = require('../middlewares/deviceCreationValidation');
 const adminRoutesMiddleware = require('../middlewares/rejectRoute');
 
 const router = express.Router();
 
 router.get("/register", adminRoutesMiddleware, adminController.register);
-router.post("/" , registerValidations, adminController.processAdminRegister);
-
 router.get("/login", adminController.loginForm);
-router.post("/login", loginValidations, adminController.processLogin);
-
 router.get('/logout', adminController.logout)
+router.get('/device/create', adminRoutesMiddleware,adminController.createDevice);
+router.get("/accessory/create", adminRoutesMiddleware,adminController.accessoryCreation);
+router.get('/update/:idProduct',adminController.updateOneDevice);
+
+router.post("/" , registerValidations, adminController.processAdminRegister);
+router.post("/login", loginValidations, adminController.processLogin);
+router.post("/accessory", uploadAccessoryImages.single("accessory_images"), accessoryCreationValidation,adminController.processAccessoryCreation);
+router.post("/device", uploadDeviceImages.array("device_images"), deviceCreationValidation,adminController.processDeviceCreation);
+
+/* router.put('/update',adminController.processOneDeviceUpdate); */
+
+router.delete('/delete/:idProduct',adminController.destroyOneDevice);
+router.delete('/delete/:idProduct',adminController.destroyOneAccessory);
+router.delete('/delete/:idProduct',adminController.destroyOneAccessory);
 
 module.exports = router;
