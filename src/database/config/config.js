057f28a614a8b34b4ec3c8022008a34f51df7570
joaquincataldo3 <joaco.cataldo3@gmail.com>
2023-03-10 10:53:14 -0500

    env variables

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,16 +1,17 @@
-const DB_USER =  process.env.DB_USER || 'root'
-const DB_PASSWORD =  process.env.DB_PASSWORD || ''
-const DB_HOST =  process.env.DB_HOST || '127.0.0.1'
-const DB_DATABASE = process.env.DB_DATABASE || 'quilmac_db'
-const DB_PORT = process.env.DB_PORT || 3306
+const DB_USER =  process.env.DB_USER
+const DB_PASSWORD =  process.env.DB_PASSWORD 
+const DB_HOST =  process.env.DB_HOST 
+const DB_DATABASE = process.env.DB_DATABASE 
+const DB_PORT = process.env.DB_PORT 
 
 
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "quilmac_db",
-    "host": "127.0.0.1",
+    "username": DB_USER,
+    "password": DB_PASSWORD,
+    "database": DB_DATABASE,
+    "host": DB_HOST,
+    "port": DB_PORT,
     "dialect": "mysql"
   },
   "test": {
