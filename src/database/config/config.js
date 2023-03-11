eb4e537cabdb456a217fe8ed2061983ce97880d2
joaquincataldo3 <joaco.cataldo3@gmail.com>
2023-03-10 19:26:24 -0500

    changes in submenu dropdown and single product

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,23 +1,22 @@
-const DB_USER =  process.env.DB_USER
-const DB_PASSWORD =  process.env.DB_PASSWORD 
-const DB_HOST =  process.env.DB_HOST 
-const DB_DATABASE = process.env.DB_DATABASE 
-const DB_PORT = process.env.DB_PORT 
+const DB_USER =  process.env.DB_USER || 'root'
+const DB_PASSWORD =  process.env.DB_PASSWORD || ''
+const DB_HOST =  process.env.DB_HOST || '127.0.0.1'
+const DB_DATABASE = process.env.DB_DATABASE || 'quilmac_db'
+const DB_PORT = process.env.DB_PORT || 3306
 
 
 module.exports = {
   "development": {
-    "username": DB_USER,
-    "password": DB_PASSWORD,
-    "database": DB_DATABASE,
-    "host": DB_HOST,
-    "port": DB_PORT,
+    "username": "root",
+    "password": null,
+    "database": "quilmac_local",
+    "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
     "password": null,
-    "database": "quilmac_db",
+    "database": "quilmac_local",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
