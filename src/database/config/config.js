3c98a8c0be296e76ff64b5bc1bd962d4ed0ba9c9
joaquin <joaco.cataldo3@gmail.com>
2023-04-05 12:20:05 -0300

    node env conf database and delete prod

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -4,13 +4,21 @@ const DB_HOST =  process.env.DB_HOST || '127.0.0.1'
 const DB_DATABASE = process.env.DB_DATABASE || 'quilmac_db'
 const DB_PORT = process.env.DB_PORT || 3306
 
+const dotenv = require("dotenv").config().parsed
+
+const DEV_DB = dotenv.DEV_DB
+const DEV_DB_HOST = dotenv.DEV_DB_HOST
+const DEV_DB_PASSWORD = dotenv.DEV_DB_PASSWORD
+const DEV_DB_PORT = dotenv.DEV_DB_PORT
+const DEV_DB_USER = dotenv.DEV_DB_USER
 
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "quilmac_local",
-    "host": "127.0.0.1",
+    "username": DEV_DB_USER,
+    "password": DEV_DB_PASSWORD,
+    "database": DEV_DB,
+    "host": DEV_DB_HOST,
+    'port': DEV_DB_PORT,
     "dialect": "mysql"
   },
   "test": {
