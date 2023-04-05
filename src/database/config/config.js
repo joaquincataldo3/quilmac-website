b54d842df52b6feb615b0ad27e03aa91239519b6
joaquin <joaco.cataldo3@gmail.com>
2023-04-05 13:05:01 -0300

    env variables

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,24 +1,27 @@
+const dotenv = require("dotenv").config().parsed
+
 const DB_USER =  process.env.DB_USER || 'root'
 const DB_PASSWORD =  process.env.DB_PASSWORD || ''
 const DB_HOST =  process.env.DB_HOST || '127.0.0.1'
 const DB_DATABASE = process.env.DB_DATABASE || 'quilmac_db'
 const DB_PORT = process.env.DB_PORT || 3306
 
-const dotenv = require("dotenv").config().parsed
 
-const DEV_DB = dotenv.DEV_DB
-const DEV_DB_HOST = dotenv.DEV_DB_HOST
-const DEV_DB_PASSWORD = dotenv.DEV_DB_PASSWORD
-const DEV_DB_PORT = dotenv.DEV_DB_PORT
-const DEV_DB_USER = dotenv.DEV_DB_USER
+const devDb = dotenv.DEV_DB
+const devDbHost = dotenv.DEV_DB_HOST
+const devDbPassword = dotenv.DEV_DB_PASSWORD
+const devDbPort = dotenv.DEV_DB_PORT
+const devDbUser = dotenv.DEV_DB_USER
+
+console.log(devDb)
 
 module.exports = {
   "development": {
-    "username": DEV_DB_USER,
-    "password": DEV_DB_PASSWORD,
-    "database": DEV_DB,
-    "host": DEV_DB_HOST,
-    'port': DEV_DB_PORT,
+    "username": devDbUser,
+    "password": devDbPassword,
+    "database": devDb,
+    "host": devDbHost,
+    'port': devDbPort,
     "dialect": "mysql"
   },
   "test": {
