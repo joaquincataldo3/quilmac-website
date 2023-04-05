34c935df014db12c3c7a5c541f2167516bde37ce
joaquin <joaco.cataldo3@gmail.com>
2023-04-05 16:40:33 -0300

    config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,10 +1,10 @@
 const dotenv = require("dotenv").config().parsed
 
-const DB_USER =  process.env.DB_USER || 'root'
-const DB_PASSWORD =  process.env.DB_PASSWORD || ''
-const DB_HOST =  process.env.DB_HOST || '127.0.0.1'
-const DB_DATABASE = process.env.DB_DATABASE || 'quilmac_db'
-const DB_PORT = process.env.DB_PORT || 3306
+const DB_USER =  process.env.DB_USER 
+const DB_PASSWORD =  process.env.DB_PASSWORD 
+const DB_HOST =  process.env.DB_HOST 
+const DB_DATABASE = process.env.DB_DATABASE 
+const DB_PORT = process.env.DB_PORT 
 
 
 const devDb = dotenv.DEV_DB
@@ -13,7 +13,7 @@ const devDbPassword = dotenv.DEV_DB_PASSWORD
 const devDbPort = dotenv.DEV_DB_PORT
 const devDbUser = dotenv.DEV_DB_USER
 
-console.log(devDb)
+
 
 module.exports = {
   "development": {
