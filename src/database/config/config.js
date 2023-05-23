96aed34c4465c2596d6880768ad905442198e361
joaquin <joaco.cataldo3@gmail.com>
2023-05-23 12:34:52 -0300

    env variables

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,11 +1,11 @@
 const dotenv = require('dotenv')
 dotenv.config()
 
-const DB_USER =  process.env.DB_USER 
-const DB_PASSWORD =  process.env.DB_PASSWORD 
-const DB_HOST =  process.env.DB_HOST 
-const DB_DATABASE = process.env.DB_DATABASE 
-const DB_PORT = process.env.DB_PORT 
+const DB_USER =  process.env.MYSQLUSER
+const DB_PASSWORD =  process.env.MYSQLPASSWORD
+const DB_HOST =  process.env.MYSQLHOST 
+const DB_DATABASE = process.env.MYSQLDATABASE
+const DB_PORT = process.env.MYSQLPORT
 
 
 module.exports = {
